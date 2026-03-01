8929ec6293297ad2b14ad49c118d8350eb0b5d52
Sultan Rafi <sultan.rafi091@gmail.com>
2026-03-02 00:59:04 +0600

    feat: Create components page with filtering, search, pagination, and API data fetching with caching.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/main/:path*",
+        destination: "http://localhost:8000/:path*",
+      },
+      {
+        source: "/api/b2c/:path*",
+        destination: "http://localhost:8001/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
